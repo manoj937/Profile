0bf8d3eb406e90293b63d1c39eed8a0e6c7c62da
manoj <mano.selvamr@gmail.com>
2020-08-01 12:40:29 +0530

    projects update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,9 +9,20 @@ export class ProjectsComponent implements OnInit {
 
   projects = [
     {
+      project: 'astrology',
+      name: 'DHARSHINI ASTROLOGICAL RESEARCH CENTER',
+      images: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png'],
+      domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
+    },{
       project: 'alrich',
       name: 'ALRICH UK CONSULTANCY',
-      images: ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg', '7.jpeg'],
+      images: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'],
+      domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
+    },
+    {
+      project: 'nightowl',
+      name: 'NIGHTOWLS STUDIO',
+      images: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'],
       domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
     },
     {
@@ -19,17 +30,16 @@ export class ProjectsComponent implements OnInit {
       name: 'STARTUP',
       images: ['1.png', '2.png', '3.png', '4.png'],
       domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
+    },{
+      project: 'livefree',
+      name: 'LIVEFREE',
+      images: ['1.png'],
+      domain: ['html', 'sass', 'ts', 'angular']
     },
     {
       project: 'innovationsquad',
       name: 'INNOVATION SQUAD',
       images: ['1.png', '2.png', '3.png'],
-      domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
-    },
-    {
-      project: 'nightowl',
-      name: 'NIGHTOWLS STUDIO',
-      images: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'],
       domain: ['html', 'sass', 'ts', 'bootstrap', 'angular']
     }
   ];
